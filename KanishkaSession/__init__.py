3ad82c5a3d50dc7b7c8ab9d6327c5de6b326867a
CoderXHitler <arank3010@gmail.com>
2024-03-13 23:59:51 -0700

    Krishna

---
+++ KanishkaSession/__init__.py
@@ -0,0 +1,60 @@
+import os
+import asyncio
+import logging
+from config import Config
+from pyrogram import Client
+from rich.console import Console
+from rich.table import Table
+from KanishkaSession.Helpers.data import LOG_TEXT,ART
+from pyromod import listen 
+
+#getting variables
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+TOKEN = Config.TOKEN
+START_PIC = Config.START_PIC
+CHAT = Config.CHAT
+
+
+if not START_PIC:
+    START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg"
+
+#rich
+LOG = Console()
+
+#logger
+logging.basicConfig(level=logging.INFO)
+
+#client
+app = Client(
+    "SupremeStark",
+    api_id = API_ID,
+    api_hash = API_HASH,
+    bot_token = TOKEN )
+    
+
+
+async def KanishkaSession():
+    os.system("clear")
+    header = Table(show_header=True, header_style="bold green")
+    header.add_column(LOG_TEXT)
+    LOG.print(header)
+    LOG.print(f"[bold cyan]{ART}")
+    LOG.print("[bold yellow]sᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ɴᴏᴡ.......")
+    await app.start()    
+    
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(KanishkaSession())    
+
+
+
+    
+    
+
+    
+    
+
+
+
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    API_ID = "24049626"
+    API_HASH = "680a4153538eb9f9a2195cbcb464114f"
+    TOKEN = "6534790446:AAE3wgX4sBp1rC_HVmTn10XtGUdhX6mtYVQ"  
+    START_PIC = "https://graph.org/file/8d7fb0c77c0ab1920cfe3.jpg" 
+    CHAT = "-1001994490527"    
